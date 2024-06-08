16c81531f7142ca9b357ecb5c018e971284108d0
PaterIntellectus <nik0121@mail.ru>
2024-06-08 14:12:58 +0500

    Create Ticket Storage
    TicketService main function got arrayList of Tickets

--- src/com/andersen/TicketService/TicketService.java
+++ src/com/andersen/TicketService/TicketService.java
@@ -1,21 +1,31 @@
 package com.andersen.TicketService;
 
-public class TicketService {
-    public static void main(String[] args) {
-        var emptyTicket = new Ticket();
-        var fullTicket = new Ticket(
-                "14R2",
-                "Hall #1",
-                "242",
-                true,
-                Ticket.StadiumSector.C,
-                4.3,
-                12.3
-        );
-        var limitedTicket = new Ticket("Hall #3", "923");
+import java.util.ArrayList;
 
-        System.out.println("Empty  : " + emptyTicket);
-        System.out.println("Full   : " + fullTicket);
-        System.out.println("Limited: " + limitedTicket);
+public class TicketService {
+
+
+
+    public static void main(String[] args) {
+        final ArrayList<Ticket> tickets = new ArrayList<Ticket>(10);
+
+        tickets.add(new Ticket());
+        tickets.get(0).setId("null");
+        tickets.add(new Ticket("R2D2", "SW Hall #1", "327", false, Ticket.StadiumSector.A, 12, 49.90));
+        tickets.add(new Ticket("GHA4", "SW Hall #1", "327", false, Ticket.StadiumSector.B, 8, 29.90));
+        tickets.add(new Ticket("Opera Hall", "419"));
+        tickets.add(new Ticket("GH2K", "SW Hall #2", "327", false, Ticket.StadiumSector.C, 4, 12.90));
+
+        tickets.add(new Ticket("5TFU", "SW Hall #1", "327", false, Ticket.StadiumSector.A, 12, 49.90));
+        tickets.add(new Ticket("Opera Hall", "419"));
+        tickets.add(new Ticket());
+        tickets.get(7).setId("AR2D");
+        tickets.add(new Ticket("LK89", "SW Hall #2", "327", false, Ticket.StadiumSector.C, 4, 12.90));
+        tickets.add(new Ticket());
+
+
+        for (final var ticket : tickets) {
+            System.out.println(ticket);
+        }
     }
 }

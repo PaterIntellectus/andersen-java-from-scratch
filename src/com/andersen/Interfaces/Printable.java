e8a747f69ea6e93327d867e71db9e71607e11a16
PaterIntellectus <nik0121@mail.ru>
2024-06-14 00:17:57 +0500

    Fix Printable and Shareable
    Made functions of both interfaces public

--- src/com/andersen/Interfaces/Printable.java
+++ src/com/andersen/Interfaces/Printable.java
@@ -1,7 +1,7 @@
 package com.andersen.Interfaces;
 
 public interface Printable {
-    default void print() {
+    public default void print() {
         System.out.println("This class is printable, please override the initial functionality of 'print' method in your class");
     }
 }
--- src/com/andersen/Interfaces/Shareable.java
+++ src/com/andersen/Interfaces/Shareable.java
@@ -1,11 +1,11 @@
 package com.andersen.Interfaces;
 
 public interface Shareable {
-    default void share(String phoneNumber) {
+    public default void share(String phoneNumber) {
         System.out.printf("The class shared by the phone number - %s\n", phoneNumber);
     }
 
-    default void share(String phoneNumber, String email) {
+    public default void share(String phoneNumber, String email) {
         System.out.printf("The class shared by the phone number - '%s'\n and an email - '%s'\n", phoneNumber, email);
     }
 }

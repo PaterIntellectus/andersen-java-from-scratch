f8d1a80bd1795669434bf474564cb7241cabff3f
PaterIntellectus <nik0121@mail.ru>
2024-06-14 00:37:42 +0500

    Refactor Shareable
    A little refactor of prints in methods

--- src/com/andersen/Interfaces/Shareable.java
+++ src/com/andersen/Interfaces/Shareable.java
@@ -2,10 +2,10 @@ package com.andersen.Interfaces;
 
 public interface Shareable {
     default void share(String phoneNumber) {
-        System.out.printf("The class shared by the phone number - %s\n", phoneNumber);
+        System.out.printf("The class shared via phone: %s\n", phoneNumber);
     }
 
     default void share(String phoneNumber, String email) {
-        System.out.printf("The class shared by the phone number - '%s'\n and an email - '%s'\n", phoneNumber, email);
+        System.out.printf("The class shared via phone: '%s', and email: '%s'\n", phoneNumber, email);
     }
 }

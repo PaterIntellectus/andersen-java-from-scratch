f5b44322d4adc34a35bb4db5b9ead30025ea7397
PaterIntellectus <nik0121@mail.ru>
2024-06-16 12:51:05 +0500

    Refactor TicketService to work with int id of Ticket

--- src/main/java/com/andersen/TicketService/TicketService.java
+++ src/main/java/com/andersen/TicketService/TicketService.java
@@ -20,16 +20,18 @@ public class TicketService {
         ticketStorage.add(new Ticket(5, "SW Hall #2", "327", false, Ticket.StadiumSector.C, 4, 12.90));
         ticketStorage.add(new Ticket());
 
+        ticketStorage.add(new Ticket(6, null,null,false,null,0,0));
+
         System.out.println("Ticket Storage:");
         for (final var ticket : ticketStorage) {
             System.out.println(ticket);
         }
 
         System.out.println("Tickets by id:");
-        System.out.printf("Ticket with id = 'R2D2': %s\n", getTicketByID(ticketStorage, "R2D2"));
-        System.out.printf("Ticket with id = 'null': %s\n", getTicketByID(ticketStorage, "null"));
-        System.out.printf("Ticket with id = 'FB2A': %s\n", getTicketByID(ticketStorage, "FB2A"));
-        System.out.printf("Ticket with id = 'null': %s\n", getTicketByID(ticketStorage, null));
+        System.out.printf("Ticket with id = 1: %s\n", getTicketByID(ticketStorage, 1));
+        System.out.printf("Ticket with id = 4: %s\n", getTicketByID(ticketStorage, 4));
+        System.out.printf("Ticket with id = 2: %s\n", getTicketByID(ticketStorage, 2));
+        System.out.printf("Ticket with id = 3: %s\n", getTicketByID(ticketStorage, 3));
     }
 
     static Ticket getTicketByID(Collection<Ticket> ticketStorage, int id) {

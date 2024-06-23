d161befa9a9f5b95424749a194e1ecca93b1cffb
PaterIntellectus <nik0121@mail.ru>
2024-06-23 23:16:53 +0500

    refactor: make getTicketByID() return Optional
    Optional type makes control over returned ticket easier
    
    Made ticketStorage initialization prettier using Arrays.asList
    Refactored main prints to use Optionals
    getTicketByID() now able to find  id = 0

--- src/main/java/com/andersen/TicketService/TicketService.java
+++ src/main/java/com/andersen/TicketService/TicketService.java
@@ -1,49 +1,44 @@
 package com.andersen.TicketService;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Objects;
+
+import java.util.*;
 
 public class TicketService {
     public static void main(String[] args) {
-        final var ticketStorage = new ArrayList<Ticket>(10);
-
-        ticketStorage.add(new Ticket());
-        ticketStorage.add(new Ticket(1, "SW Hall #1", "327", false, Ticket.StadiumSector.A, 12, 49.90));
-        ticketStorage.add(new Ticket(2, "SW Hall #1", "327", false, Ticket.StadiumSector.B, 8, 29.90));
-        ticketStorage.add(new Ticket("Opera Hall", "419"));
-        ticketStorage.add(new Ticket(3, "SW Hall #2", "327", false, Ticket.StadiumSector.C, 4, 12.90));
-
-        ticketStorage.add(new Ticket(4, "SW Hall #1", "327", false, Ticket.StadiumSector.A, 12, 49.90));
-        ticketStorage.add(new Ticket("Opera Hall", "419"));
-        ticketStorage.add(new Ticket());
-        ticketStorage.add(new Ticket(5, "SW Hall #2", "327", false, Ticket.StadiumSector.C, 4, 12.90));
-        ticketStorage.add(new Ticket());
-
-        ticketStorage.add(new Ticket(6, null,null,false,null,0,0));
+        final var ticketStorage = new ArrayList<Ticket>(Arrays.asList(
+                new Ticket(1, "SW Hall #1", "327", false, Ticket.StadiumSector.A, 12, 49.90),
+                new Ticket(),
+                new Ticket(2, "SW Hall #1", "327", false, Ticket.StadiumSector.B, 8, 29.90),
+                new Ticket("Opera Hall", "419"),
+                new Ticket(3, "SW Hall #2", "327", false, Ticket.StadiumSector.C, 4, 12.90),
+                new Ticket(4, "SW Hall #1", "327", false, Ticket.StadiumSector.A, 12, 49.90),
+                new Ticket("Opera Hall", "419"),
+                new Ticket(),
+                new Ticket(5, "SW Hall #2", "327", false, Ticket.StadiumSector.C, 4, 12.90),
+                new Ticket()
+        ));
 
         System.out.println("Ticket Storage:");
         for (final var ticket : ticketStorage) {
             System.out.println(ticket);
         }
 
-        System.out.println("Tickets by id:");
-        System.out.printf("Ticket with id = 1: %s\n", getTicketByID(ticketStorage, 1));
-        System.out.printf("Ticket with id = 4: %s\n", getTicketByID(ticketStorage, 4));
-        System.out.printf("Ticket with id = 2: %s\n", getTicketByID(ticketStorage, 2));
-        System.out.printf("Ticket with id = 3: %s\n", getTicketByID(ticketStorage, 3));
+        System.out.println("Getting Tickets by id:");
+        for (final var id : List.of(1, 4, 0, 2, 4, 23)) {
+            final var ticket = getTicketByID(ticketStorage, id);
+
+            System.out.printf(
+                    "Ticket with id = %d: %s\n", id, ticket.isPresent() ? ticket.get() : "no such ticket!"
+            );
+        }
     }
 
-    static Ticket getTicketByID(Collection<Ticket> ticketStorage, int id) {
+    static private Optional<Ticket> getTicketByID(Collection<Ticket> ticketStorage, int id) {
         for (final var ticket : ticketStorage) {
-            if (ticket.getId() != 0 && ticket.getId() == id) {
-                return ticket;
-            } else {
-                if (Objects.equals(ticket.getId(), id)) {
-                    return ticket;
-                }
+            if (ticket.getId() == id) {
+                return Optional.of(ticket);
             }
         }
-        return null;
+        return Optional.empty();
     }
 }

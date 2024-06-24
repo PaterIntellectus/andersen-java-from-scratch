f145ac183584bdfcc91d90e9f2c16f8f06d750e1
PaterIntellectus <nik0121@mail.ru>
2024-06-24 23:21:48 +0500

    feature: create template class for future implementation of ArrayList

---
+++ src/main/java/com/andersen/core/ArrayList.java
@@ -0,0 +1,21 @@
+package com.andersen.core;
+
+public class ArrayList<Item> {
+
+    public void put(final Item item) {
+
+    };
+
+    public Item get(final int index) {
+        return null;
+    }
+
+    public void delete(final int index) {
+
+    }
+
+    private void resize() {
+
+    }
+
+}
